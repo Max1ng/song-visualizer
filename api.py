feb075042123417eb6ec2ebf5c21904a8968f69e
Max Toth <maxjtoth@gmail.com>
2023-09-17 02:46:29 -0700

    initial commit

---
+++ api.py
@@ -0,0 +1,76 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post
+import json
+import requests
+
+class api:
+
+#get IDs from .env file
+    load_dotenv()
+
+    def __init__(self) -> None:
+        self.clientID = os.getenv("CLIENT_ID")
+        self.clientSecret = os.getenv("CLIENT_SECRET")
+        self.token = self.getToken()
+
+        self.artist_name = "khalid"
+        self.songs = self.getArtistSongs(self.artist_name, self.token)
+
+
+    def getToken(self):
+        authString = self.clientID + ":" + self.clientSecret
+        authBytes = authString.encode("utf-8")
+        authBase64 = str(base64.b64encode(authBytes), "utf-8")
+
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization": "Basic " + authBase64,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        data = {"grant_type": "client_credentials"}
+        result = post(url, headers=headers, data=data)
+        jsonResult = json.loads(result.content)
+        token = jsonResult["access_token"]
+        return token
+
+    def getAuthHeader(self, token):
+        return {"Authorization": "Bearer " + self.token}
+
+
+
+    
+    #print(self.token)
+
+
+
+    def getArtistSongs(self, artist_name, token):
+        base_url = "https://api.spotify.com/v1/search"
+        headers = self.getAuthHeader(token)
+
+        params = {
+            "q": f"artist:{artist_name}",
+            "type": "track",
+            "limit": 50  # You can adjust the limit as needed
+        }
+
+        try:
+            response = requests.get(base_url, headers=headers, params=params)
+            response.raise_for_status()
+            data = response.json()
+
+            # Extract and return the list of tracks
+            if "tracks" in data and "items" in data["tracks"]:
+                return data["tracks"]["items"]
+            else:
+                return []
+
+        except requests.exceptions.RequestException as e:
+            print("Error:", e)
+            return []
+
+    # Print the list of songs
+    def printSongs(self):
+        for song in self.songs:
+            print(song["name"])
---
+++ main.py
@@ -0,0 +1,10 @@
+from api import api
+
+#rocket?
+
+
+if __name__ == "__main__":
+    api = api()
+    print(api.token)
+    api.printSongs()
+
